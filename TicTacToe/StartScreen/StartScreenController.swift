8813e4eb05ac5976f688c076adba84fc70cb5e61
Valentina Popova <info@valentina-popova.ru>
2024-10-01 14:24:58 +0300

    Save

--- TicTacToe/StartScreen/StartScreenController.swift
+++ TicTacToe/StartScreen/StartScreenController.swift
@@ -6,11 +6,3 @@
 //
 
 import UIKit
-
-class StartViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .red
-    }
-}
