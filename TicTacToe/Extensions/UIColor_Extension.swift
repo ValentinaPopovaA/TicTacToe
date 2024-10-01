28d7e45a2ec9357b10a651cfd95ba1b17a51c2d2
Dmitry Zyadik <dmitry.zyadik@gmail.com>
2024-10-01 13:44:16 +0500

    add enum Image & fix UIColor Extension

--- TicTacToe/Extensions/UIColor_Extension.swift
+++ TicTacToe/Extensions/UIColor_Extension.swift
@@ -7,91 +7,15 @@
 
 import UIKit
 
-enum AppColors {
-    case basic_white
-    case basic_background
-    case basic_black
-    case basic_blue
-    case basic_light_blue
-    case secondary_purple
-    case secondary_pink
-    case secondary_gray
-    
-    var color: UIColor? {
-        switch self {
-        case .basic_blue:
-            return UIColor(named: "blue" )
-        case .basic_light_blue:
-            return UIColor(named: "light_blue")
-        case .basic_white:
-            return UIColor(named: "white" )
-        case .basic_background:
-            return UIColor(named: "background" )
-        case .basic_black:
-            return UIColor(named: "black" )
-        case .secondary_purple:
-            return UIColor(named: "purple" )
-        case .secondary_pink:
-            return UIColor(named: "pink" )
-        case .secondary_gray:
-            return UIColor(named: "gray" )
-        }
-    }
-}
 extension UIColor {
- 
-    
-    static func appColor(_ name: AppColors) -> UIColor? {
-        switch name {
-        case .basic_blue:
-            return UIColor(named: "blue" )
-        case .basic_light_blue:
-            return UIColor(named: "light_blue")
-        case .basic_white:
-            return UIColor(named: "white" )
-        case .basic_background:
-            return UIColor(named: "background" )
-        case .basic_black:
-            return UIColor(named: "black" )
-        case .secondary_purple:
-            return UIColor(named: "purple" )
-        case .secondary_pink:
-            return UIColor(named: "pink" )
-        case .secondary_gray:
-            return UIColor(named: "gray" )
-        }
-    }
-   
-    static var basic_white: UIColor {
-        return AppColors.basic_blue.color!
-    }
-    
-    static var basic_background: UIColor {
-        return AppColors.basic_background.color!
-    }
-    
-    static var basic_light_blue: UIColor {
-        return AppColors.basic_light_blue.color!
-    }
-    
-    static var basic_blue: UIColor {
-        return AppColors.basic_blue.color!
-    }
-    
-    static var basic_black: UIColor {
-        return AppColors.basic_black.color!
-    }
-    
-    static var secondary_purple: UIColor {
-        return AppColors.secondary_purple.color!
-    }
-    
-    static var secondary_pink: UIColor {
-        return AppColors.secondary_pink.color!
-    }
-    
-    static var secondary_gray: UIColor {
-        return AppColors.secondary_gray.color!
-    }
+    // Basic
+    static let basic_white = UIColor(named: "white" )
+    static let basic_background = UIColor(named: "background" )
+    static let basic_light_blue = UIColor(named: "light_blue")
+    static let basic_blue = UIColor(named: "blue")
+    static let basic_black = UIColor(named: "black")
+    // Secondary
+    static let secondary_purple = UIColor(named: "purple")
+    static let secondary_pink = UIColor(named: "pink")
+    static let secondary_gray = UIColor(named: "gray")
 }
-
