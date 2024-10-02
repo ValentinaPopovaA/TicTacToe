b7ddc1c9cd93aace602505a6be05af45095155c1
Alisa <alisa.bajenova@gmail.com>
2024-10-02 15:49:58 +0300

    3 button added

--- TicTacToe/SelectedGame/SelectedGameController.swift
+++ TicTacToe/SelectedGame/SelectedGameController.swift
@@ -8,58 +8,36 @@
 import UIKit
 
 class SelectedGameController: UIViewController {
-    let selectGameView = UIView()
     let selectGameStackView = UIStackView()
-    let selectGameLabel = UILabel()
-    var singlePlayerButton = UIButton(type: .custom)
-    var twoPlayersButton = UIButton(type: .custom)
+    var selectGameView: UIView = {
+        let view = UIView()
+        view.configureView(color: .white, applyShadow: true)
+        return view
+    }()
+    var selectGameLabel = UILabel.createLabel(text: "Select Game", fontSize: 24, isBold:true)
+    var singlePlayerButton = UIButton.makeCustomButtonWithImage(image: UIImage(named: "single_player"), label:"Single Player", target: SelectedGameController.self, action: #selector(goToNextScreen))
+    var twoPlayersButton = UIButton.makeCustomButtonWithImage(image: UIImage(named: "two_players"), label: "Two Players", target: SelectedGameController.self, action: #selector(goToNextScreen))
+    var leaderbordButton = UIButton.makeCustomButtonWithImage(image: UIImage(named: "rocket"), label: "Leaderboard", target: SelectedGameController.self, action: #selector(goToNextScreen))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .basic_background
-        
-        selectGameView.layer.backgroundColor = UIColor.basic_white.cgColor
-        selectGameView.layer.cornerRadius = 30
+
         view.addSubview(selectGameView)
-        selectGameView.layer.shadowColor = UIColor(red: 0.604, green: 0.624, blue: 0.765, alpha: 0.3).cgColor
-        selectGameView.layer.shadowRadius = 30
-        selectGameView.layer.shadowOpacity = 1
-        selectGameView.layer.shadowOffset = CGSize(width: 4, height: 4)
-        
-        selectGameLabel.text = "Select Game"
-        selectGameLabel.font = .systemFont(ofSize: 24, weight: .bold)
-        selectGameLabel.textAlignment = .center
-        selectGameLabel.textColor = .basic_black
-        
-        singlePlayerButton = UIButton.makeCustomButtonWithImage(
-            image: UIImage(named: "single_player"),
-            label: "Single Player",
-            target: self,
-            action: #selector(singlePlayerButtonTapped)
-        )
-        singlePlayerButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: -4)
-        singlePlayerButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -4, bottom: 0, right: 4)
-        
-        twoPlayersButton = UIButton.makeCustomButtonWithImage(
-            image: UIImage(named: "two_players"),
-            label: "Two Players",
-            target: self,
-            action: #selector(twoPlayersButtonTapped)
-        )
-        twoPlayersButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 6, bottom: 0, right: -6)
-        twoPlayersButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -6, bottom: 0, right: 6)
+
         
         selectGameStackView.addArrangedSubview(selectGameLabel)
         selectGameStackView.addArrangedSubview(singlePlayerButton)
         selectGameStackView.addArrangedSubview(twoPlayersButton)
+        selectGameStackView.addArrangedSubview(leaderbordButton)
         selectGameStackView.axis = .vertical
         selectGameStackView.spacing = 20
         
         makeConstraints()
+    
     }
     
     func makeConstraints() {
-        selectGameView.translatesAutoresizingMaskIntoConstraints = false
         selectGameStackView.translatesAutoresizingMaskIntoConstraints = false
         selectGameView.addSubview(selectGameStackView)
         
@@ -74,15 +52,21 @@ class SelectedGameController: UIViewController {
             selectGameStackView.bottomAnchor.constraint(equalTo: selectGameView.bottomAnchor, constant: -20),
             
             singlePlayerButton.heightAnchor.constraint(equalToConstant: 69),
-            twoPlayersButton.heightAnchor.constraint(equalToConstant: 69)
+            twoPlayersButton.heightAnchor.constraint(equalToConstant: 69),
+            leaderbordButton.heightAnchor.constraint(equalToConstant: 69)
         ])
     }
     
-    @objc func singlePlayerButtonTapped() {
-        print ("single")
+//    func setViews() {
+//            view.addSubview(selectGameStackView)
+//            view.addSubview(selectGameView)
+//            view.addSubview(selectGameLabel)
+//            view.addSubview(singlePlayerButton)
+//            view.addSubview(twoPlayersButton)
+//        }
+    @objc func goToNextScreen() {
+        let vc = NextVC()
+        navigationController?.pushViewController(vc, animated: true)
     }
-    
-    @objc func twoPlayersButtonTapped() {
-        print ("two")
     }
-}
+
