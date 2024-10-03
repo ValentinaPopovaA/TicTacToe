5112cf1155505ad72993a8a5420db3fa4f588fe5
Nikita Moskalev <nikmosyl@gmail.com>
2024-10-03 21:59:38 +0400

    add game setting service

---
+++ TicTacToe/Services/GameService.swift
@@ -0,0 +1,17 @@
+//
+//  GameService.swift
+//  TicTacToe
+//
+//  Created by nik on 03.10.24.
+//
+
+import Foundation
+
+enum Move {
+    case circle
+    case cross
+}
+
+final class GameService {
+    
+}
