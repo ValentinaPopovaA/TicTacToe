a29bd96a47d28a770830e1a7465e467bfb743735
Valentina Popova <info@valentina-popova.ru>
2024-10-04 23:35:32 +0300

    Delete minimax

--- TicTacToe/Services/GameService.swift
+++ TicTacToe/Services/GameService.swift
@@ -99,90 +99,12 @@ final class GameService {
         if let winningMove = findWinningMove(for: .playerTwo) {
             return winningMove
         }
+
         if let blockingMove = findWinningMove(for: .playerOne) {
             return blockingMove
         }
-        return minimaxMove()
-    }
-    
-    private func minimaxMove() -> Int? {
-        var bestScore = Int.min
-        var bestMove: Int? = nil
-        
-        for move in availableCells {
-            makeMove(at: move, for: .playerTwo)
-            let score = minimax(isMaximizing: false)
-            undoMove(at: move)
-            
-            if score > bestScore {
-                bestScore = score
-                bestMove = move
-            }
-        }
-        
-        return bestMove
-    }
-    
-    private func minimax(isMaximizing: Bool) -> Int {
-        if let winner = checkWinner() {
-            switch winner {
-            case .playerOne:
-                return -10
-            case .playerTwo:
-                return 10
-            default:
-                return 0
-            }
-        }
-        
-        if isMaximizing {
-            var bestScore = Int.min
-            for move in availableCells {
-                makeMove(at: move, for: .playerTwo)
-                let score = minimax(isMaximizing: false)
-                undoMove(at: move)
-                bestScore = max(score, bestScore)
-            }
-            return bestScore
-        } else {
-            var bestScore = Int.max
-            for move in availableCells {
-                makeMove(at: move, for: .playerOne)
-                let score = minimax(isMaximizing: true)
-                undoMove(at: move)
-                bestScore = min(score, bestScore)
-            }
-            return bestScore
-        }
-    }
-    
-    // Симуляция выполнения хода
-    private func makeMove(at index: Int, for player: Turn) {
-        availableCells.remove(index)
-        if player == .playerOne {
-            playerOneCombination.insert(index)
-        } else {
-            playerTwoCombination.insert(index)
-        }
-    }
-    
-    // Отмена хода
-    private func undoMove(at index: Int) {
-        availableCells.insert(index)
-        playerOneCombination.remove(index)
-        playerTwoCombination.remove(index)
-    }
-    
-    // Проверка победителя
-    private func checkWinner() -> Turn? {
-        for combination in winningCombinations {
-            if combination.isSubset(of: playerOneCombination) {
-                return .playerOne
-            } else if combination.isSubset(of: playerTwoCombination) {
-                return .playerTwo
-            }
-        }
-        return nil
+
+        return chooseOptimalMove()
     }
     
     private func findWinningMove(for player: Turn) -> Int? {
@@ -197,5 +119,17 @@ final class GameService {
         }
         
         return nil
+    }
+    
+    private func chooseOptimalMove() -> Int? {
+        if availableCells.contains(5) {
+            return 5
+        }
+
+        if let corner = Set([1, 3, 7, 9]).intersection(availableCells).randomElement() {
+            return corner
+        }
+
+        return availableCells.randomElement()
     }
 }
