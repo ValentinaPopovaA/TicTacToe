f737c802fbaba9e5c12041496bd1a04f81e051b0
Nikita Moskalev <nikmosyl@gmail.com>
2024-10-04 20:23:11 +0400

    add two player mode

--- TicTacToe/Game/GameScreenViewController.swift
+++ TicTacToe/Game/GameScreenViewController.swift
@@ -7,15 +7,31 @@
 
 import UIKit
 
+struct Player {
+    let image: UIImage
+    let name: String
+}
+
 final class GameScreenViewController: UIViewController {
     
     private let gameScreenView = GameScreenView()
-    private let gameService: GameService
+    private var gameService: GameService
     
     private let gameSettings = GameSettings.shared.getSettingsLoad()
     
+    private let player1: Player
+    private let player2: Player
+    
     init(gameMode: GameMode, gameDifficulty: GameDifficulty) {
         self.gameService = GameService(gameMode: gameMode, gameDifficulty: gameDifficulty)
+        player1 = Player(
+            image: UIImage(named: gameSettings.player1Image) ?? UIImage.pair1Cross,
+            name: gameService.gameMode == .singlePlayer ? "You" : "Player One"
+        )
+        player2 = Player(
+            image: UIImage(named: gameSettings.palyer2Image) ?? UIImage.pair1Circle,
+            name: gameService.gameMode == .singlePlayer ? "Computer" : "Player Two"
+        )
         
         super.init(nibName: nil, bundle: nil)
     }
@@ -35,12 +51,26 @@ final class GameScreenViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        gameScreenView.configureBages(
-            imageOne: UIImage(named: gameSettings.player1Image) ?? UIImage.singlePlayer,
-            nameOne: gameService.gameMode == .singlePlayer ? "You" : "Player One",
-            imageTwo: UIImage(named: gameSettings.palyer2Image) ?? UIImage.rocket,
-            nameTwo: gameService.gameMode == .singlePlayer ? "Computer" : "Player Two"
+        gameService = GameService(
+            gameMode: gameService.gameMode,
+            gameDifficulty: gameService.gameDifficulty
         )
+        
+        gameScreenView.configureBages(
+            imageOne: player1.image,
+            nameOne: player1.name,
+            imageTwo: player2.image,
+            nameTwo: player2.name
+        )
+        gameScreenView.setMove(player1.image, "\(player1.name) turn")
+        
+        for tag in 1...9 {
+            if let button = gameScreenView.viewWithTag(tag) as? UIButton {
+                button.setImage(nil, for: .normal)
+            }
+        }
+        
+        enableBoard()
     }
     
     private func performComputerMove() {
@@ -49,15 +79,17 @@ final class GameScreenViewController: UIViewController {
 
             if let computerMove = self.gameService.getComputerMove() {
                 if let button = self.gameScreenView.viewWithTag(computerMove) as? UIButton {
-                    button.setImage(UIImage.pair1Cross, for: .normal)
+                    button.setImage(player2.image, for: .normal)
                     button.isUserInteractionEnabled = false
 
-                    let result = self.gameService.moveMade(by: .cross, at: computerMove)
+                    let result = self.gameService.moveMade(at: computerMove)
 
                     if let gameResult = result {
                         self.handleGameResult(gameResult)
-                    } else {
+                    }
+                    else {
                         self.enableBoard()
+                        self.showTurn()
                     }
                 }
             }
@@ -67,8 +99,7 @@ final class GameScreenViewController: UIViewController {
     private func handleGameResult(_ result: RoundResult) {
         let resultVC = ResultController()
         resultVC.gameResult = result
-        resultVC.modalPresentationStyle = .fullScreen
-        present(resultVC, animated: true, completion: nil)
+        navigationController?.pushViewController(resultVC, animated: true)
     }
     
     private func disableBoard() {
@@ -92,22 +123,26 @@ final class GameScreenViewController: UIViewController {
 
 extension GameScreenViewController: GameScreenViewDelegate {
     func cellPressed(_ sender: UIButton) {
-        print("Tapped cell № \(sender.tag)")
-
         disableBoard()
+        
+        let currentPlayer = gameService.currentTurn == .playerOne ? player1 : player2
+        sender.setImage(currentPlayer.image, for: .normal)
+        
 
-        sender.isUserInteractionEnabled = false
-        sender.setImage(UIImage.pair1Circle, for: .normal)
-
-        let result = gameService.moveMade(by: .circle, at: sender.tag)
-
-        if let gameResult = result {
-            handleGameResult(gameResult)
+        if let result = gameService.moveMade(at: sender.tag) {
+            handleGameResult(result)
         } else if gameService.gameMode == .singlePlayer {
+            showTurn()
             performComputerMove()
         } else {
+            showTurn()
             enableBoard()
         }
+    }
+    
+    func showTurn() {
+        let currentPlayer = gameService.currentTurn == .playerOne ? player1 : player2
+        gameScreenView.setMove(currentPlayer.image, "\(currentPlayer.name) turn")
     }
 }
 
